a743f5d860cce5494d2154ccb7cee1bdc510146a
bereket <emailbereket@gmail.com>
2023-01-05 22:29:21 -0800

    task zero

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,3 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[idx]), end="")
         except IndexError:
             break
-        idx += 1
-    print("")
-    return 
