ac3baab0339ad435ab2774b78b29177efeca9e29
berialx <emailbereket@gmail.com>
2023-03-31 02:23:51 -0700

    task 1 corrected

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""
-send POST req url, given URL & email as params. Display response body utf-8
-usage
-"""
-from sys import argv
-import urllib.parse
+"""Response header value"""
 import urllib.request
-
+import sys
 
 if __name__ == "__main__":
-    url = argv[1]
-    values = {'email': argv[2]}
-
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(sys.argv[1]) as url:
+        s = url.getheader('X-Request-Id')
+        print("{}".format(s))
