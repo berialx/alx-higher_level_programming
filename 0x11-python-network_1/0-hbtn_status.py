1f72efa2ecc7fa7540df6d7c6f5aef6145cb931b
berialx <emailbereket@gmail.com>
2023-03-31 02:25:47 -0700

    task 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-fetch https://intranet.hbtn.io/status
-"""
-
+"""What's my status?"""
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
+    s = url.read()
+    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(req.format(type(s), s, s.decode('utf-8')))
